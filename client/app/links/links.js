ab5c103b7d017866aa950c27064b725472f8632e
Hack Reactor Students <robb.mitch+github@gmail.com>
2014-07-02 17:31:41 -0700

    pass 13 tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,14 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+
+  $scope.data = {};
+  $scope.getLinks = function(){
+    Links.get()
+    .then(function(resp) {
+        $scope.data.links = resp.data;
+    });
+  };
+  $scope.getLinks();
+  //  = [{},{},{}];
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,8 +1,28 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
+  var chicken = {};
+  chicken.get = function() {
+    return $http({
+    method: 'GET',
+    url: '/api/links'
+    });
+  };
+  chicken.post = function(link){
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: link
+    })
+    .then(function(resp){
+      return resp.data;
+    });
+  }
+
+  return chicken;
 })
+
+
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user
